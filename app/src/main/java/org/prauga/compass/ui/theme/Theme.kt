2db10a83032b77e9eb16780571f77db9edf659ba
Saalim Quadri <danascape@gmail.com>
2026-02-11 20:54:00 +0530

    feat: Switch to semantic versioning (#12)
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- app/src/main/java/org/prauga/compass/ui/theme/Theme.kt
+++ app/src/main/java/org/prauga/compass/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package org.prauga.compass.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
